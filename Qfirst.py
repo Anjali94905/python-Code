392854210a098553f9d874611335389b1ccc7a8c
Anjali94905 <Anjalidevi94905@gmail.com>
2024-10-19 17:46:23 +0530

    practice question

---
+++ Qfirst.py
@@ -0,0 +1,18 @@
+# input 2 numbers & print their sum
+first =int (input("enter first : "))
+second =int (input("enter second : "))
+print("sum=", first + second)
+
+# input side of a square & print its area
+side =float (input("enter square side : "))
+print("area=", side * side)
+
+#input 2 number & print their average
+first =int (input("enter first : "))
+second =int (input("enter second : "))
+print((first + second)/2)
+
+# input 2 int numbers ,a and b print true if A is greater to B .if not print false
+a =int (input("enter a : "))
+b =int (input("enter b : "))
+print(a>=b)
--- first.py
+++ first.py
@@ -1,1 +1,19 @@
-print("Hello world!somechanges ")
+#Calculate Simple Interest.
+p = float(input("p:"))
+r = float(input("r:"))
+t = float(input("t:"))
+si = (p*r*t)/100
+print(si)
+#Arithmetic Operators
+a = 5
+b = 2
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a % b)
+print(a ** b)
+
+
+
+
