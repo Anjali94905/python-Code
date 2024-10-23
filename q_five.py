f38049fab72cb04d396dc3fd202fe0dd05162dd3
Anjali94905 <Anjalidevi94905@gmail.com>
2024-10-23 09:06:48 +0530

    list commit

--- q_five.py
+++ q_five.py
@@ -1,16 +1,16 @@
 
 # (Q1) WAP to print the length of a list.
-'''cities = ["delhi", "gurgaon", "noida", "pune","mumbai", "chennai"]
+cities = ["delhi", "gurgaon", "noida", "pune","mumbai", "chennai"]
 heroes = ["thor", "ironman", "captain america", "shaktiman"]
 
 def print_len(list):
     print(len(list))
 
 print_len(cities)
-print_len(heroes)'''
+print_len(heroes)
 
 # (Q2) WAP to print the element of a list in a single line.
-'''cities = ["delhi", "gurgaon", "noida", "pune","mumbai", "chennai"]
+cities = ["delhi", "gurgaon", "noida", "pune","mumbai", "chennai"]
 heroes = ["thor", "ironman", "captain america", "shaktiman"]
 
 def print_len(list):
@@ -22,16 +22,16 @@ def print_list(list):
 
 print_list(cities)
 print_list(heroes)
-print()'''
+print()
 
 #(Q3) WAF to find the factorial of n.
-'''def cal_fact(n):
+def cal_fact(n):
     fact = 1
     for i in range(1, n+1):
         fact *= i
     print(fact)
 
-cal_fact(3)'''
+cal_fact(3)
 
 # (Q4) WAF to convert USD to INR. 
 def converter(usd_val):
